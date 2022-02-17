bef3f00869e5b94529c298c5b5931c032d9cc6de
Butrint Metaj <butrint.metaj@gmail.com>
2022-02-17 22:12:28 +0100

    Add delete friend_requests callback when friendship idelete is commited to db

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,11 @@
 class Friendship < ApplicationRecord
+  after_commit :remove_friend_request, on: :destroy
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def remove_friend_request
+    FriendRequest.where(requester_id: user_id, requestee_id: friend_id)
+                 .or(FriendRequest.where(requester_id: friend_id, requestee_id: user_id)).destroy_all
+  end
+
 end
--- db/migrate/20220217153834_create_friendships.rb
+++ db/migrate/20220217153834_create_friendships.rb
@@ -3,6 +3,7 @@ class CreateFriendships < ActiveRecord::Migration[7.0]
     create_table :friendships do |t|
       t.references :user, null: false, foreign_key: true
       t.references :friend, null: false
+      t.index [:user_id, :friend_id], unique: true
       t.timestamps
     end
     add_foreign_key :friendships, :users, column: :friend_id
