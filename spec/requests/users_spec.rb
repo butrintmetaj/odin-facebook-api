44765aa1347a01ab9c81a690f495b9c3ef1c5ae3
Butrint Metaj <butrint.metaj@gmail.com>
2022-02-22 14:26:38 +0100

    Create users_spec.rb

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  let!(:user) { create(:user) }
+  let!(:friendships) { create_list(:friendship, 2, user_id: user.id) }
+  let!(:users) { create_list(:user, 2) }
+  let(:auth_header) { auth_with_token(user) }
+
+  describe 'GET /users' do
+    it 'returns a list of users that current user is not friends' do
+      get '/api/v1/users', headers: auth_header
+
+      body = JSON.parse(response.body)['data']
+
+      expect(response).to have_http_status(:ok)
+      expect(body.size).to eq(users.size)
+    end
+
+    describe 'GET /users/:id' do
+      it 'returns a list of users that current user is not friends' do
+        get "/api/v1/users/#{users.first.id}", headers: auth_header
+
+        body = JSON.parse(response.body)['data']
+
+        expect(response).to have_http_status(:ok)
+        expect(body['attributes']['id']).to eq(users.first.id)
+        expect(body['attributes']['email']).to eq(users.first.email)
+      end
+    end
+  end
+end
